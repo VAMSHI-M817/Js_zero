a9cca77817ef07ceb4979928a5432199104ab658
VAMSHI-M817 <vamshimarepu@gmail.com>
2024-04-02 23:04:53 +0530

    updating dom

--- DOM/script-4.js
+++ DOM/script-4.js
@@ -1,45 +1,69 @@
 //innerHtml && CreateElement        
 //There is some methods Quick & Dirty 
 function createListItem(item) {
-    const li = document.createElement('li')
-    li.innerHTML = `${item}
+  const li = document.createElement('li')
+  li.innerHTML = `${item}
     <button class="remove-item btn-link text-red">
       <i class="fa-solid fa-xmark"></i>
     </button>`
 
-    document.querySelector('.items').appendChild(li)
+  document.querySelector('.items').appendChild(li)
 }
 
 
 //Clean & Performant
 
-function creatListItem(item){ //created an item in function parameter
+function creatListItem(item) { //created an item in function parameter
 
-    //Creating li text here
-    const li = document.createElement('li')
-    
-    //Inserting the text into li using "createTextNode"
-    li.appendChild(document.createTextNode(item))
+  //Creating li text here
+  const li = document.createElement('li')
 
-    //Creating the button here
-    const button = document.createElement('button')
-    //Applying class to the button 
-    button.className = 'remove-item btn-link text-red'
+  //Inserting the text into li using "createTextNode"
+  li.appendChild(document.createTextNode(item))
 
-    //Creating the icon
-    const icon = document.createElement('i')
-    //Applying the class to it
-    icon.className = 'fa-solid fa-xmark'
 
-    //Here we are inserting icon into button
-    button.appendChild(icon)
+  const button = createButton('remove-item btn-link text-red')
 
-    //Here we are inserting button into li to create an item
-    li.appendChild(button)
 
-    //Here we're selecting the "ul" class and appending the li into ul
-    document.querySelector('.items').appendChild(li)
+  //Here we are inserting icon into button
+  button.appendChild(icon)
+
+//Here we are inserting button into li to create an item
+li.appendChild(button)
+
+
+  //Here we're selecting the "ul" class and appending the li into ul
+  document.querySelector('.items').appendChild(li)
 }
 
 //Passing parameter to it
 creatListItem('Vamshi')
+
+//Refactor to Multiple Functions
+
+function createButton(classes) {
+  //Creating the button here
+  const button = document.createElement('button')
+  //Applying class to the button 
+  button.className = classes
+
+  const icon = createIcon('fa-solid fa-xmark')
+  button.appendChild(icon)
+  
+  return button
+}
+
+function createIcon(classes) {
+
+  //Creating the icon
+  const icon = document.createElement('i')
+  //Applying the class to it
+  icon.className = classes
+
+  return icon
+}
+
+
+
+
+createListItem('Mango')
