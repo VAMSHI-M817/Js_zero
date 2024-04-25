2576c1ed531555feacc3c9555e768cce0030ac4b
vamshi_git1024 <vamshi9850@gmail.com>
2024-04-25 01:18:28 -0700

    Updated Bubble_Sort Algorithm

---
+++ Sorting_Methods/Bubble_Sort.js
@@ -0,0 +1,24 @@
+
+
+
+function bubbleSort(array) {
+    const n = array.length
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (arr[j] > arr[j + 1]) {
+                //swap
+                const temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+
+            }
+        }
+    }
+
+}
+
+const arr = [5, 4, 3, 2, 1]
+console.log("Unsorted", arr)
+bubbleSort(arr)
+console.log(arr) 
