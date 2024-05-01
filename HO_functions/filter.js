3765e26a0966df82ac3ba7155fe02b985c5bd591
VAMSHI-M817 <vamshimarepu817@gmail.com>
2024-05-01 18:19:10 +0530

    aa

--- HO_functions/filter.js
+++ HO_functions/filter.js
@@ -29,3 +29,15 @@ const filter3 = arr.filter((x) => x > 6);
 console.log(filter);
 console.log(filter2);
 console.log(filter3);
+
+//Filtering the users by age
+
+const users = [
+  { firstName: "vamshi", lastName: "M", age: 41 },
+  { firstName: "akshay", lastName: "M", age: 22 },
+  { firstName: "tajmul", lastName: "M", age: 23 },
+  { firstName: "sohail", lastName: "M", age: 20 },
+];
+
+const output = users.filter((x) => x.age < 30).map((x) => x.firstName);
+console.log(output);
