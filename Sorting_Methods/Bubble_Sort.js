ef1ca3d39f2b04f72e8a32d4535360fc82ce6e1e
VAMSHI-M817 <vamshimarepu817@gmail.com>
2024-05-05 01:37:16 +0530

    ll

--- Sorting_Methods/Bubble_Sort.js
+++ Sorting_Methods/Bubble_Sort.js
@@ -12,9 +12,8 @@ function bubbleSort(array) {
                 const temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
-
             }
-        }   
+        }
     }
 
 }
@@ -22,6 +21,22 @@ function bubbleSort(array) {
 const arr = [5, 4, 3, 2, 1]
 console.log("Unsorted", arr)
 bubbleSort(arr)
-//array sorted
+//Array sorted
 //2 Contributing update
-console.log(arr) 
+console.log("Sorted", arr)
+
+
+let array = [1, 2, 3, 4, 5]
+
+for (let i = 0; i < array.length; i++) {
+    // const element = array[i];
+    // console.log(element);
+    for (let j = 0; j < array.length; j++) {
+        const element2 = array[j];
+        // console.log(array[j]);
+        for (let k = 0; k < array.length; k++) {
+            const element3 = array[k];
+            console.log(array[k]);
+        }
+    }
+}
