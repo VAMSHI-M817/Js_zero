ce09ec40f33f50a4af3c09b8561292b790a43a75
VAMSHI-M817 <vamshimarepu817@gmail.com>
2024-07-04 00:18:27 +0530

    ...

--- String_Methods/str_methods.js
+++ String_Methods/str_methods.js
@@ -19,7 +19,36 @@ console.log(user.indexOf("v"));
 console.log(0 === "0");
 console.log("0" == 0);
 
-let arr = ["vamshi", "shiva", "radha"];
-for (i = 0; i <= arr.length; i++) {
-    log
+let arr = [
+    user1 = {
+        uname: "vamshi",
+        email: "vamsi@gmail.com"
+    },
+    user2 = {
+        uname: "ramu",
+        email: "ramu@gmail.com"
+    },
+    user3 = {
+        uname: "shiva",
+        email: "shiva@gmail.com"
+    },
+    user4 = {
+        uname: "raju",
+        email: "raju@gmail.com"
+    },
+];
+for (i = 0; i <= arr.length - 1; i++) {
+    const element = arr[i]
+    console.log(element)
 }
+
+let nums = [5, 6, 3, 6, 7, 8, 23]
+console.log(nums.sort((a, b) => {
+    return a - b
+}))
+
+const newarr = ["vamsi", "vamsi", "vamsi", "vamsi",]
+const newarr2 = ["shiva", "shiva", "shiva", "shiva",]
+
+console.log(newarr)
+console.log(...newarr2)
