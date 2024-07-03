ebc4bc569ed5c3badcd2ad5a08971a2be868930a
VAMSHI-M817 <vamshimarepu817@gmail.com>
2024-07-03 17:48:16 +0530

    ...

---
+++ OOPS/challenges.js
@@ -0,0 +1,41 @@
+//Finding largest number using ifelse
+const findNum = (a, b, c) => {
+  if (a >= b && a >= c) {
+    return a;
+  } else if (b >= a && b >= c) {
+    return b;
+  } else {
+    return c;
+  }
+};
+console.log("Largest Num:", findNum(1415, 1415, 566));
+
+//Finding largest number using Math method
+const findNum2 = (a, b, c) => {
+  return Math.max(a, b, c);
+};
+console.log("Largest Num:", findNum2(14243, 1424, 1234));
+
+//Finding largest number in array
+let arr = [12, 3434, 34, 1234, 1223535, 1234234234];
+const findNum3 = (data) => {
+  return Math.max(...data);
+};
+console.log("Largest Num", findNum3(arr));
+
+//finding largest number using ternary operator
+const findNum4 = (a, b, c) => {
+  return a <= b && a <= c ? a : b <= a && b <= c ? b : c;
+};
+console.log("Largest Num:", findNum4(20, 21, 22));
+
+//adding all numbers
+const arr1 = [1, 2, 3];
+const add = (data) => {
+  for (let i = 0; i <= data.length; i++) {
+    let count = 0;
+    return (count += data[i]);
+  }
+};
+const added = add(arr1);
+console.log(added);
