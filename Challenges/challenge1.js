8aef84394cc3e4d3e41ce85f585ada631960ee10
VAMSHI-M817 <vamshimarepu817@gmail.com>
2024-07-16 00:49:13 +0530

    ....

--- Challenges/challenge1.js
+++ Challenges/challenge1.js
@@ -1,60 +1,50 @@
-"use strict";
-//adding Last name
-
-let firstName = ["Vamshi", "Ramu", "Shiva", "Raju"];
-let lastName = "Zenoids";
-
-// for (let i = 0; i < firstName.length; i++) {
-//   const Fname = firstName[i];
-//   let newArray = [];
-//   newArray.push(`${Fname} ${lastName}`);
-//   console.log(newArray);
-// }
-
-//calculate total
-let gas = [300, 400, 800, 500];
-let food = [3000, 4000, 8000, 5000];
-
-const Total = (data) => {
-  let TA = 0;
-  for (let i = 0; i < data.length; i++) {
-    TA += data[i];
-    if (TA > 3000) {
-      console.log("You're spending too much");
-    } else {
-      console.log("You're good in savings");
-    }
-  }
-  return TA;
-};
-
-// const Tgas = Total(gas);
-// const Tfood = Total(food);
-// console.log({
-//   TotalGasCost: Tgas,
-//   TotalFoodCost: Tfood,
-// });
 
 const users = [
   {
     id: 1,
-    name: "vamshi",
+    uname: "shivani",
     email: "vamshi@gmail",
   },
   {
     id: 2,
-    name: "vamshi",
+    uname: "vamshi",
     email: "vamshi@gmail",
   },
   {
     id: 3,
-    name: "shiva",
+    uname: "shiva",
     email: "shiva@gmail",
   },
   {
     id: 4,
-    name: "raju",
+    uname: "raju",
     email: "raju@gmail",
   },
 ];
 
+users.filter(filtering)
+
+function filtering(eachItem) {
+  const { id, uname, email } = eachItem
+  eachItem.id % 2 === 0 ? console.log(uname) : false
+}
+
+
+class Person {
+  constructor(firstName, lastName, fullName) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.fullName = function () {
+      return this.firstName + this.lastName
+    }
+  }
+
+  sample(fullName) {
+    console.log(fullName)
+  }
+  
+
+
+
+}
+const vamshi = new Person("vamshi", "Marepu")
